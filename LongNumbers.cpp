50b5cabef7c442e7c1568ecd172996795886f54e
igarek <igarekkkkk@mail.ru>
2013-03-27 12:41:26 +0200

    Update LongNumbers.cpp

--- LongNumbers.cpp
+++ LongNumbers.cpp
@@ -15,6 +15,7 @@ int len;// length
         LongNumbers(int newlen, char * newnum);// 
         LongNumbers(const LongNumbers &x);
         ~LongNumbers();// destructor
+        double t();
         
         LongNumbers &operator=(const LongNumbers &Object); // Перегрузка =
         LongNumbers operator+(LongNumbers &); // Додавання цілих довгих чисел
@@ -23,6 +24,11 @@ int len;// length
         LongNumbers operator**(LongNumbers &); // Множення довгого цілого числа на довге
         LongNumbers operator%(LongNumbers &); // Ділення довгого цілого числа на коротке
         LongNumbers operator%%(LongNumbers &); // Ділення довгого цілого числа на довге
+        
+          // Перегрузка оператора << для вивода
+        friend ostream &operator<<(ostream &, LongNumbers &);//дружні оператори
+        // Перегрузка оператора >> для ввода масиву
+        friend istream &operator>>(istream &, LongNumbers &);//дружні оператори
          
 }
 
@@ -31,7 +37,28 @@ LongNumbers::LongNumbers(int newlen, char * newnum)
 len=strlen(num);
 number=new char[len];
 strncpy(numb,len);//??
-} 
+
+    unsigned char i;///////// розбирааааааааю, сделаю
+    len=newlen;
+    num=new char[n+1];
+    for(i=0;i<=n;x[i++]=xx[i]);
+}
+ 
+polynom::polynom()
+{
+    double x[1]={0};
+    polynom::polynom(0,x);
+}
+ 
+polynom::~polynom()
+{
+    delete []x;
+}
+ 
+double polynom::t()
+{
+    return x[0];
+}
 
 LongNumbers::LongNumbers()// constructors
         {
